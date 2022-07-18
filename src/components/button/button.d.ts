6ec1f849748b502e7443c3d66ada1d94b71ab7c1
Ishaan Puniani <ishaan.puniani@kindredgroup.com>
2022-07-18 11:25:56 +0200

    inital test commit

---
+++ src/components/button/button.d.ts
@@ -0,0 +1,4 @@
+export declare const Button: ({ label, onClick }: {
+    label: any;
+    onClick: any;
+}) => import("lit-html").TemplateResult<1>;
---
+++ src/components/button/button.stories.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    title: string;
+    argTypes: {
+        label: string;
+        onClick: {
+            action: string;
+        };
+    };
+};
+export default _default;
+export declare const Primary: any;
