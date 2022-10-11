6695a92210e01ffe4238f73ef1ad888e6286cf82
Ishaan Puniani <ishaan_puniani@yahoo.com>
2022-10-11 13:17:53 +0200

    added chart library

--- src/lib/simulator/Simulator.tsx
+++ src/lib/simulator/Simulator.tsx
@@ -1,13 +1,70 @@
 import React from "react";
+// import the core library.
+import ReactEChartsCore from "echarts-for-react/lib/core";
+// Import the echarts core module, which provides the necessary interfaces for using echarts.
+import * as echarts from "echarts/core";
+// Import charts, all with Chart suffix
+import { BarChart } from "echarts/charts";
+// import components, all suffixed with Component
+import {
+  GridComponent,
+  TooltipComponent,
+  TitleComponent,
+  DatasetComponent,
+} from "echarts/components";
+// Import renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
+import {
+  CanvasRenderer,
+  // SVGRenderer,
+} from "echarts/renderers";
+
+// Register the required components
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  BarChart,
+  CanvasRenderer,
+]);
+
+const options = {
+  grid: { top: 8, right: 8, bottom: 24, left: 36 },
+  xAxis: {
+    type: "category",
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: "bar",
+      smooth: true,
+    },
+  ],
+  tooltip: {
+    trigger: "axis",
+  },
+};
 
 export interface ISimulatorProps {
   text?: string;
 }
 
 const Simulator = (props: ISimulatorProps) => {
+  console.log("REACHED");
   return (
     <>
       <h1>{props.text}</h1>
+      <h2>Done</h2>
+      <ReactEChartsCore
+        echarts={echarts}
+        option={options}
+        notMerge={true}
+        lazyUpdate={true}
+        theme={"theme_name"}
+      />
     </>
   );
 };
