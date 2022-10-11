907635c06d37567126b027b120165878adeeb78b
Ishaan Puniani <ishaan_puniani@yahoo.com>
2022-10-11 13:33:25 +0200

    removed build

--- build/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-declare class MyLibrary {
-    constructor();
-    myMethod: () => boolean;
-    initSimulator: (container: any) => void;
-}
-export default MyLibrary;
--- build/types/simulator/Simulator.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const Simulator: () => JSX.Element;
-export default Simulator;
