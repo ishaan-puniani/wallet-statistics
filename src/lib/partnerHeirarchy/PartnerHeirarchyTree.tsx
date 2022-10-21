7c90e375d94d1f5e9fd8bead0bd8b807ab81bc98
kushal0891 <kushal.singh@coderower.com>
2022-10-21 12:14:48 +0530

    Added tree view for partner hierachy

--- src/lib/partnerHeirarchy/PartnerHeirarchyTree.tsx
+++ src/lib/partnerHeirarchy/PartnerHeirarchyTree.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
+import { ExpandMore, ChevronRight } from "@mui/icons-material";
+import { TreeItem, TreeView } from "@mui/lab";
 export interface IPartnerHeirarchyTreeProps {
   credentials?: any;
   partnerId?: string;
@@ -12,7 +13,7 @@ const PartnerHeirarchyTree = (props: IPartnerHeirarchyTreeProps) => {
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-      const heirarchyResponse = await axios.post(
+      const heirarchyResponse = await axios.get(
         `https://wallet-and-bonus-47kby.ondigitalocean.app/api/tenant/${props.credentials.application_id}/partners-hierarchy/${props.partnerId}`,
         {
           ...props.credentials,
@@ -30,7 +31,28 @@ const PartnerHeirarchyTree = (props: IPartnerHeirarchyTreeProps) => {
       <h2>PartnerHeirarchyTree : {props.partnerId}</h2>
 
       {loading && <h1>Loading</h1>}
-      {!loading && <div>{JSON.stringify(heirarchy)}</div>}
+      {/* {!loading && <div>{JSON.stringify(heirarchy)}</div>} */}
+      {!loading &&
+        heirarchy.map((record) => (
+          <TreeView
+            aria-label="file system navigator"
+            defaultCollapseIcon={<ExpandMore />}
+            defaultExpandIcon={<ChevronRight />}
+          >
+            <TreeItem nodeId={record.partnerId} label={record.partnerName}>
+            <div>
+              <ul>
+                <li>Id - {record.partnerId}</li>
+                <li>Level - {record.levelName}</li>
+                <li>Children Count - {record.childrenCount}</li>
+                <li> Email - {record.additionalData.email}</li>
+                <li>Full Name - {record.additionalData.fullName}</li>
+                <li>Number - {record.additionalData.phoneNumber}</li>
+              </ul>
+            </div>
+            </TreeItem>
+          </TreeView>
+        ))}
 
       <p>{JSON.stringify(props.credentials)}</p>
     </>
