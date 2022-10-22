41783f230bdda14a60482b960d42f31293dac4b9
Ishaan Puniani <ishaan_puniani@yahoo.com>
2022-10-22 11:52:31 +0200

    removed creds

--- src/credentials.ts
+++ src/credentials.ts
@@ -1,5 +1,1 @@
-export const credentials = {
-  application_id: "ac95e373-81c9-4b71-a683-db0359840ae4",
-  client_id: "REPORT_READER@ac95e373-81c9-4b71-a683-db0359840ae4.wab",
-  client_secret: "48e26a0e-98f9-42dc-a9a0-36d63f8acd61",
-};
+export const credentials = {};
