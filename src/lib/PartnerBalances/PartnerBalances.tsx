c5059e1709bf46ed5f26e10793305c81041d2111
kushal0891 <kushal.singh@coderower.com>
2022-10-25 18:23:07 +0530

    Partner Balances

---
+++ src/lib/PartnerBalances/PartnerBalances.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./partner_balances.css";
+export interface IPartnerBalancesProps {
+  credentials?: any;
+  userId?: string;
+  currency?: "COINS" | "USD";
+}
+
+const PartnerBalances = (props: IPartnerBalancesProps) => {
+  const [loading, setLoading] = useState(false);
+  const [balance, setBalance] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const fetchBalance = await axios.post(
+        `https://wallet-and-bonus-47kby.ondigitalocean.app/api/tenant/${props.credentials.application_id}/get-current-balances-for-transaction-types?filter[userId]=${props.userId}&filter[currency]=${props.currency}`
+      );
+      if (fetchBalance.data) {
+        setBalance(fetchBalance.data);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [props.userId, props.currency]);
+  return (
+    <>
+      <h2>Partner Balances : {props.userId}</h2>
+
+      {loading && <h1>Loading</h1>}
+      <div className="wrapper">
+        {!loading &&
+          balance.map((record) => (
+            <div className="card">
+              <p><strong>Transaction Type</strong> :  {record.transactionType.slice(0,17)}</p>
+              <p ><strong>Amount</strong> : {parseFloat(record.amount).toFixed(2)}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+export default PartnerBalances;
---
+++ src/lib/PartnerBalances/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PartnerBalances from "./PartnerBalances";
+export const render = (container: any) => {
+  ReactDOM.render(<PartnerBalances />, container);
+};
