22b88167177949fe4bfdccaef474e058b96b35a6
Ishaan Puniani <ishaan_puniani@yahoo.com>
2022-12-10 00:35:51 -0800

    updated packaging

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,7 @@
 import "./index.css";
-import PartnerHeirarchyTree from "./partnerHeirarchy/PartnerHeirarchyTree";
 import { render } from "./simulator";
 
 class WalletStatistics {
-  public static PartnerHeirarchyTree = PartnerHeirarchyTree;
-
   constructor() {
     console.log("Library constructor loaded");
   }
@@ -16,7 +13,6 @@ class WalletStatistics {
   initSimulator = (container: any) => {
     render(container);
   };
- 
 }
 
 export default WalletStatistics;
