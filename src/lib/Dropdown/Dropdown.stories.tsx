3f980b0d35fe3d7c48014b691df9691c3f69fd54
Rashida1akter <rashida.akter@coderower.com>
2023-03-25 14:17:30 +0600

    add dropdown

---
+++ src/lib/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { getMachineUserCredentials } from "../../utilities/storage";
+import DropdownCurrencyTypes from "./DropdownCurrencyTypes";
+import DropdownTransactionTypes from "./DropdownTransactionTypes";
+
+export default {
+  title: "Example/Dropdown",
+  component: DropdownCurrencyTypes,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof DropdownCurrencyTypes>;
+
+const Template: ComponentStory<typeof DropdownCurrencyTypes> = (args) => (
+  <DropdownCurrencyTypes {...args} />
+);
+export const currencyTypes = Template.bind({});
+currencyTypes.args = {
+  credentials: getMachineUserCredentials() || {},
+};
+const TemplateTransactionTypes: ComponentStory<
+  typeof DropdownTransactionTypes
+> = (args) => <DropdownTransactionTypes {...args} />;
+export const TransactionTypes = TemplateTransactionTypes.bind({});
+TransactionTypes.args = {
+  credentials: getMachineUserCredentials() || {},
+};
---
+++ src/lib/Dropdown/DropdownCurrencyTypes.tsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Multiselect from "multiselect-react-dropdown";
+import { API_HOST } from "../../constants";
+export interface IDropdownProps {
+  credentials?: any;
+}
+
+const DropdownCurrencyTypes = (props: IDropdownProps) => {
+  const [loading, setLoading] = useState(false);
+  const [currencyData, setCurrencyData] = useState<any>();
+  let options = [];
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const fetchBalance = await axios.post(
+        `${API_HOST}/tenant/${props.credentials.application_id}/get-currency-autocomplete`,
+        {
+          ...props.credentials,
+        }
+      );
+      if (fetchBalance.data) {
+        const items = fetchBalance.data;
+        setCurrencyData(items);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  options = currencyData?.map(
+    (record: { [s: string]: unknown } | ArrayLike<unknown>) =>
+      Object.values(record)[1]
+  );
+  return (
+    <>
+      <h2>Currency</h2>
+      {loading && <h1>Loading</h1>}
+
+      <div className="">
+        {" "}
+        <Multiselect
+          isObject={false}
+          onKeyPressFn={function noRefCheck() {}}
+          onRemove={function noRefCheck() {}}
+          onSearch={function noRefCheck() {}}
+          onSelect={function noRefCheck() {}}
+          options={options}
+        />
+      </div>
+    </>
+  );
+};
+export default DropdownCurrencyTypes;
