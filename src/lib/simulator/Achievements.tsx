2e603d6ed83a5fd90a0348132ba48697e460fe11
Rashida1akter <rashida.akter@coderower.com>
2023-03-25 12:24:53 +0600

    add achievements form in the simulator

---
+++ src/lib/simulator/Achievements.tsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { API_HOST } from "../../constants";
+import "./stimulator.css";
+export interface IAchievements {
+  credentials?: any;
+  achieverId?: any;
+  action? : string
+  value?:number
+}
+
+const Achievements = (props: IAchievements) => {
+  console.log("REACHED");
+
+  const { register, handleSubmit } = useForm();
+console.log(props.achieverId)
+  const onSubmit = async (data: any) => {
+    const fetchBalance = await axios.post(
+      `${API_HOST}/tenant/${props.credentials.application_id}/sim-user-achievements`,
+      {
+        data,
+      }
+    );
+    console.log(fetchBalance);
+  };
+
+  return (
+    <>
+      <h1>Achievement Stimulator</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ul className="formStyle">
+          <div>
+            <li>
+              <label>Achiever Id :</label>
+              <input
+                value={props.achieverId}
+                {...register("achieverId")}
+              />
+            </li>
+            <li>
+              <label>Action : </label>
+              <input value={props.action} {...register("action")} />
+            </li>
+            <li>
+              <label>Value :</label>
+              <input value={props.value} {...register("value")} />
+            </li>
+          </div>
+        </ul>
+        <div className="formStyle">
+          <input type="submit" />
+        </div>
+      </form>
+    </>
+  );
+};
+export default Achievements;
