d93875acfe5872eba6d6ee30fa4a098e2062f35a
aremon86851 <aremon86851@gmail.com>
2023-04-05 04:41:52 +0600

    expose simulator achievement

--- src/lib/simulator/index.tsx
+++ src/lib/simulator/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Simulator from "./Simulator";
+import Achievements from "./Achievements";
 export const render = (container: any) => {
   ReactDOM.render(<Simulator />, container);
 };
+export const renderSimulatorAchievement = (container: any, props: any) => {
+  ReactDOM.render(<Achievements {...props} />, container);
+};
+
