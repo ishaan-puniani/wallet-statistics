cd680bfe0a94695cbe12fe2f1fb8e3567e199962
Kushal0891 <kushal.singh@coderower.com>
2023-04-25 19:23:12 +0530

    params fix

--- src/lib/services/achievementLogs.ts
+++ src/lib/services/achievementLogs.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { API_HOST } from "../../constants";
 
 export const _fetchAchievementsLogs = async (
+  credentials?: any,
   transaction?: string,
-  userAchievement?: string,
-  credentials?: any
+  userAchievement?: string
 ) => {
   const achievementLogs = await axios.post(
     `${API_HOST}/tenant/${credentials.application_id}/get-user-achievement-logs?limit=10&offset=0&filter[transaction]=${transaction}&filter[userAchievement]=${userAchievement}`,
