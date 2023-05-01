d15cfffa49ddf9625640b58e5acdd1a0dc6c6283
Kushal0891 <kushal.singh@coderower.com>
2023-05-01 14:11:04 +0530

    Added limit and offset in logs service

--- src/lib/services/achievementLogs.ts
+++ src/lib/services/achievementLogs.ts
@@ -4,10 +4,12 @@ import { API_HOST } from "../../constants";
 export const _fetchAchievementsLogs = async (
   credentials?: any,
   transaction?: string,
-  userAchievement?: string
+  userAchievement?: string,
+  limit?: string,
+  offset?: string
 ) => {
   const achievementLogs = await axios.post(
-    `${API_HOST}/tenant/${credentials.application_id}/get-user-achievement-logs?limit=10&offset=0&filter[transaction]=${transaction}&filter[userAchievement]=${userAchievement}`,
+    `${API_HOST}/tenant/${credentials.application_id}/get-user-achievement-logs?limit=${limit}&offset=${offset}&filter[transaction]=${transaction}&filter[userAchievement]=${userAchievement}`,
     {
       ...credentials,
     }
