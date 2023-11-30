23647396cfa698e9cbc52da6491e38cd65ac43b4
Deepak Gupta <deepak.gupta@coderower.com>
2023-11-30 13:59:34 +0530

    add note for reloading after update service acc cred

--- src/stories/ServiceAccountCredentials.tsx
+++ src/stories/ServiceAccountCredentials.tsx
@@ -3,37 +3,40 @@ import { useForm } from "react-hook-form";
 import { saveServiceAccountCredentials } from "../utilities/storage";
 import { _initialize } from "../lib/services/settings";
 
-const ServiceAccountCredentials = ({credentials}:any) => {
+const ServiceAccountCredentials = ({ credentials }: any) => {
   const { register, handleSubmit } = useForm({
-    defaultValues: credentials
+    defaultValues: credentials,
   });
   const onSubmit = async (data) => {
     saveServiceAccountCredentials(data);
     await _initialize(data);
-
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        Application Id: <input {...register("application_id")} />
-      </div>
-      <div>
-        Client Id: <input {...register("client_id")} />
-      </div>
-      <div>
-        Client Secret: <input {...register("client_secret")} />
-      </div>
-      <div>
-        version:
-        <select {...register("version")}>
-          <option value=""></option>
-          <option value="storybook_">storybook_</option>
-          <option value="v1_">v1_</option>
-        </select>
-      </div>
-      <input type="submit" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          Application Id: <input {...register("application_id")} />
+        </div>
+        <div>
+          Client Id: <input {...register("client_id")} />
+        </div>
+        <div>
+          Client Secret: <input {...register("client_secret")} />
+        </div>
+        <div>
+          version:
+          <select {...register("version")}>
+            <option value=""></option>
+            <option value="storybook_">storybook_</option>
+            <option value="v1_">v1_</option>
+          </select>
+        </div>
+        <input type="submit" />
+      </form>
+      <hr />
+      <div>Note : After Submitting, please do a Reload</div>
+    </>
   );
 };
 
