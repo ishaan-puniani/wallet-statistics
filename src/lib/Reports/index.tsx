f0b3b6f330f2ea8e94295cf270dd3fa623236523
aremon-coderower <aremon.rahman@coderower.com>
2024-02-10 18:10:51 +0600

    mini transaction card add for uses

--- src/lib/Reports/index.tsx
+++ src/lib/Reports/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ReportChart from "./ReportChart";
 import ReportBalanceChart from "./ReportBalanceChart";
+import MiniTransactionTypeCard from "./MiniTransactionTypeCard";
 
 export const _renderReportChart = (container: any, props: any) => {
   ReactDOM.render(<ReportChart {...props} />, container);
@@ -9,4 +10,8 @@ export const _renderReportChart = (container: any, props: any) => {
 
 export const _renderReportBalanceChart = (container: any, props: any) => {
   ReactDOM.render(<ReportBalanceChart {...props} />, container);
+};
+
+export const _renderMiniTransactionTypeCard = (container: any, props: any) => {
+  ReactDOM.render(<MiniTransactionTypeCard {...props} />, container);
 };
