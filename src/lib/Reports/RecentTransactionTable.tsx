0dd632542a378c7bb16ea8f3f6b2929b3a9202a1
Deepak Gupta <deepak.gupta@coderower.com>
2024-05-09 12:54:18 +0530

    fix build

--- src/lib/Reports/RecentTransactionTable.tsx
+++ src/lib/Reports/RecentTransactionTable.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { _fetchReportTransactions } from "../services/balances";
 import styled from "styled-components";
 import moment from "moment";
