98985ab758cbe1461867d9a480f1718f69274826
kushal0891 <kushal.singh@coderower.com>
2026-02-26 15:42:12 +0530

    feat: refactor BalancesChart to use line chart and improve data fetching logic

--- src/lib/PartnerBalances/BalancesChart.tsx
+++ src/lib/PartnerBalances/BalancesChart.tsx
@@ -4,25 +4,25 @@ import ReactEChartsCore from "echarts-for-react/lib/core";
 // Import the echarts core module, which provides the necessary interfaces for using echarts.
 import * as echarts from "echarts/core";
 // Import charts, all with Chart suffix
-import { BarChart, PieChart } from "echarts/charts";
+import { LineChart } from "echarts/charts";
 // import components, all suffixed with Component
 import {
   GridComponent,
-  LegendComponent,
-  TitleComponent,
   TooltipComponent,
+  TitleComponent,
 } from "echarts/components";
 // Import renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
 import {
   CanvasRenderer,
+  // SVGRenderer,
 } from "echarts/renderers";
-
-import moment from "moment";
+import axios from "axios";
+import { API_HOST } from "../../constants";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { _fetchBalanceHistory } from "../services/balances";
+import moment from "moment";
 import { getTheme, makeRandomColor } from "../../utilities/theme";
-import { balanceKeyMap, Group } from "../Reports/utils/utils";
-import { _fetchBalance, _fetchGetBalances } from "../services/balances";
 
 // Register the required components only in browser environment
 if (typeof window !== 'undefined' && typeof document !== 'undefined') {
@@ -30,257 +30,206 @@ if (typeof window !== 'undefined' && typeof document !== 'undefined') {
     TitleComponent,
     TooltipComponent,
     GridComponent,
-    BarChart,
+    LineChart,
     CanvasRenderer,
-    PieChart,
-    LegendComponent,
   ]);
 }
 
-export interface IPartnerBalancesPieChartProps {
+export interface BalanceReportChartFilterProps {
+  endDate: Date;
+  startDate: Date;
   userId: string;
   currency: string;
   credentials: any;
-  amountType: "amount" | "virtual";
-  showRaw?: boolean;
+  label: string;
+  reportKey: string;
+  showRaw: boolean;
   transactionTypes?: string[];
-  startDate?: Date;
-  endDate?: Date;
-  group?: Group;
-  includePrevious?: boolean;
-  includeToday?: boolean;
-  type?: string;
-  volume?: "total" | "group";
-  identifierMapper?: any;
-  /*
-  {
-"FOOD": {
-        "chart": {
-            "color": "green"
-        }
-    },
-    },
-    
-  */
-  chartOptions?: any;
-  themeConfig: any;
 }
 
 const option: any = {
   tooltip: {
-    trigger: "item",
+    trigger: "axis",
   },
   legend: {
-    top: "0%",
-    left: "center",
+    data: [],
   },
-
   grid: {
     left: "3%",
     right: "4%",
     bottom: "0%",
     containLabel: true,
   },
-  series: [
-    {
-      name: "",
-      type: "pie",
-      radius: ["40%", "70%"],
-      avoidLabelOverlap: false,
-      itemStyle: {
-        borderRadius: 10,
-        borderColor: "#fff",
-        borderWidth: 2,
-      },
-      label: {
-        show: false,
-        position: "center",
-      },
-      emphasis: {
-        label: {
-          show: true,
-          fontSize: 40,
-          fontWeight: "bold",
-        },
-      },
-      labelLine: {
-        show: false,
-      },
-      data: [],
-      // color: ["red", "blue"],
+  toolbox: {
+    feature: {
+      saveAsImage: {},
     },
-  ],
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: [],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [],
 };
 
-const BalancesChart = (props: IPartnerBalancesPieChartProps) => {
+const BalancesReportChart = (props: BalanceReportChartFilterProps) => {
   const [loading, setLoading] = useState(false);
   const [chartOption, setChartOption] = useState();
+
   const [rawData, setRawData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-      let balances: any = [];
+      const balanceReport: any = await _fetchBalanceHistory(
+        props.credentials,
+        props.userId,
+        props.currency,
+        props.startDate
+          ? moment(props.startDate).format("YYYY-MM-DD")
+          : moment().add(-7, "days").format("YYYY-MM-DD"),
+        moment(props.endDate).format("YYYY-MM-DD")
+      );
+      if (balanceReport) {
+        const items = balanceReport.rows;
+        setRawData(items);
+        const theme = getTheme();
 
-      if (props.startDate && props.endDate) {
-        const group = props.group ?? ("weekly" as Group);
-        const includePrevious = props.includePrevious || false;
-        const includeToday = props.includeToday || false;
-        const type = props.type || "debit";
-        const volume = props.volume || "total";
-        const volumeType = volume === "group" ? "group" : "total";
-        const virtualOrReal = props.amountType === "virtual" ? "virtual" : "real";
-        const key = balanceKeyMap?.[type]?.[virtualOrReal]?.[volumeType];
+        const allDate: Array<string> = [];
+        const legends: Array<string> = [];
+        const series: any = [];
+        items.forEach(
+          (item: {
+            virtualValues: unknown;
+            amounts: unknown;
+            date: string;
+            [key: string]: any;
+          }) => {
+            allDate.push(item.date);
+            const amounts = item[props.reportKey];
+            // if (props.amountType === "amount") {
+            //   amounts = item.amounts;
+            // }
+            // if (props.amountType === "virtual") {
+            //   amounts = item.virtualValues;
+            // }
 
-        balances = await _fetchGetBalances(
-          props.credentials,
-          props.userId,
-          props.currency,
-          moment(props.startDate).format("YYYY-MM-DD"),
-          moment(props.endDate).format("YYYY-MM-DD"),
-          group,
-          includePrevious,
-          includeToday
-        );
+            if (amounts) {
+              const xData = Object.keys(amounts);
 
-        const chartData = [] as any[];
-        const chartColors: any[] = [];
-        const theme = getTheme() || {};
-        theme.transactionTypes = props.themeConfig || theme.transactionTypes;
-        let chartOptions =
-          Object.keys(props.chartOptions || {}).length > 0
-            ? props.chartOptions
-            : option;
+              for (let idx = 0; idx < xData.length; idx++) {
+                const transactionType = xData[idx];
+                if (
+                  props.transactionTypes &&
+                  !props.transactionTypes.includes(transactionType)
+                ) {
+                  continue;
+                }
+                let seriesOfTransactionType = series.find(
+                  (s: any) => s.id === transactionType
+                );
+                if (!seriesOfTransactionType) {
+                  const transactionTypeTheme =
+                    theme.transactionTypes[transactionType];
+                  let colorForTransactionType;
 
-        const balance = balances?.[0]?.[key] ?? {};
-        if (Object.keys(balance).length) {
-          setRawData(balances);
-          for (let transactionType in balance) {
-            if (
-              props.transactionTypes &&
-              !props.transactionTypes.includes(transactionType)
-            ) {
-              continue;
-            }
-            const transactionTypeTheme = theme.transactionTypes[transactionType];
-            let colorForTransactionType = transactionTypeTheme?.chart?.color;
-            if (!colorForTransactionType) {
-              colorForTransactionType = makeRandomColor();
-            }
-            chartColors.push(colorForTransactionType);
-            const bal = balance[transactionType];
-            chartData.push({
-              value: Math.abs(bal),
-              name: props.identifierMapper
-                ? props.identifierMapper[transactionType]
-                : transactionType,
-              transactionTypes: transactionType,
-            });
-          }
-          chartOptions.series[0].data = chartData;
-          chartOptions.series[0].color = chartColors;
-        } else {
-          chartOptions.series[0].data = [];
-          chartOptions.series[0].color = [];
-        }
+                  if (transactionTypeTheme) {
+                    colorForTransactionType = transactionTypeTheme.chart.color;
+                  }
 
-        setChartOption(chartOptions);
-      } else {
-        // fallback to legacy per-transaction-type balances
-        balances = await _fetchBalance(
-          props.credentials,
-          props.userId,
-          props.currency
-        );
+                  if (!colorForTransactionType) {
+                    colorForTransactionType = makeRandomColor();
+                  }
 
-        if (balances) {
-          setRawData(balances);
-          const chartData = [], chartColors = [];
+                  legends.push(transactionType);
+                  seriesOfTransactionType = {
+                    id: transactionType,
+                    name: transactionType,
+                    type: "line",
+                    data: allDate.length > 1 ? Array(allDate.length) : [], // >1 because first dats is just added in the same loop
+                    color: colorForTransactionType,
+                  };
+                  series.push(seriesOfTransactionType);
+                }
 
-          const theme = getTheme() || {};
-          theme.transactionTypes = props.themeConfig || theme.transactionTypes;
-          let chartOptions;
-          chartOptions = Object.keys(props.chartOptions || {}).length
-            ? props.chartOptions
-            : option;
-          for (let idx = 0; idx < balances.length; idx++) {
-            const balnce = balances[idx];
-            if (
-              props.transactionTypes &&
-              !props.transactionTypes.includes(balnce.transactionType)
-            ) {
-              continue;
-            }
-            const transactionTypeTheme =
-              theme.transactionTypes[balnce.transactionType];
-            let colorForTransactionType;
-
-            if (transactionTypeTheme) {
-              colorForTransactionType = transactionTypeTheme.chart.color;
-            }
-
-            if (!colorForTransactionType) {
-              colorForTransactionType = makeRandomColor();
-            }
-
-            chartColors.push(colorForTransactionType);
-
-            if (props.amountType === "amount") {
-              chartData.push({
-                value: Math.abs(balnce.amount),
-                name: balnce.transactionType,
-              });
-            }
-            if (props.amountType === "virtual") {
-              chartData.push({
-                value: Math.abs(balnce.virtualValue),
-                name: balnce.transactionType,
-              });
+                seriesOfTransactionType.data.push(
+                  Math.abs(parseFloat(amounts[transactionType] || 0))
+                );
+              }
             }
           }
+        );
 
-          chartOptions.series[0].data = chartData;
-          chartOptions.series[0].color = chartColors;
+        option.legend.data = legends;
+        option.xAxis.data = allDate;
+        option.series = series;
 
-          setChartOption(chartOptions);
-        }
+        setChartOption(option);
+        setLoading(false);
       }
-      setLoading(false);
     };
-    if ((props.userId, props.currency, props.amountType)) {
-      fetchData();
-    }
-  }, [props.userId, props.currency, props.amountType, props.themeConfig]);
+    fetchData();
+  }, [
+    props.userId,
+    props.reportKey,
+    props.currency,
+    props.startDate,
+    props.endDate,
+  ]);
 
-  // console.log(balance)
+  //   const uniqueArray = names?.filter((value, index) => {
+  //     return names?.indexOf(value) === index;
+  //   });
+  //   const newObjects = uniqueArray.map((item) => {
+  //     const aArrays = [
+  //       `${balance.map((ea) => {
+  //         if (ea[item]) {
+  //           return Math.abs(parseFloat(ea[item]));
+  //         } else if (ea[item] === undefined) {
+  //           return 0;
+  //         }
+  //       })}`,
+  //     ];
+  //     const strArray = aArrays[0].split(",");
+  //     const numArray = strArray
+  //       .filter((str) => str !== "")
+  //       .map((str) => Number(str));
+  //     const singleObject = {
+  //       name: item,
+  //       type: "line",
+  //       data: numArray,
+  //     };
+  //     return singleObject;
+  //   });
+
   return (
     <>
       {/* {loading && <h1>Loading</h1>} */}
-      {props?.showRaw ? (
+      {props.showRaw ? (
         <>
-          {rawData?.map((item) => (
-            <>
-              <div className="card">
-                <SyntaxHighlighter language="javascript" style={docco}>
-                  {JSON.stringify(item, null, 2)}
-                </SyntaxHighlighter>
-              </div>
-            </>
-          ))}
+          <div className="card">
+            <SyntaxHighlighter language="javascript" style={docco}>
+              {JSON.stringify(rawData, null, 2)}
+            </SyntaxHighlighter>
+          </div>
         </>
       ) : (
-        <div style={{ marginTop: "20px", height: "100%" }}>
+        <>
           {!loading && chartOption && (
             <ReactEChartsCore
               echarts={echarts}
-              option={chartOption}
+              option={option}
               notMerge={true}
               lazyUpdate={true}
             />
           )}
-        </div>
+        </>
       )}
     </>
   );
 };
-export default BalancesChart;
+export default BalancesReportChart;
